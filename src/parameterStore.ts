d938612d903742cdca65ff49ebcf4bef88d2c761
Linden Quan <linden.quan@resmed.com>
2022-04-14 14:37:47 -0300

    update

--- src/parameterStore.ts
+++ src/parameterStore.ts
@@ -1,26 +1,26 @@
-// import AWS from 'aws-sdk';
+import AWS from 'aws-sdk';
 
-// const parameterStore = new AWS.SSM();
+const parameterStore = new AWS.SSM();
 
 export default async function getParameter(parameterName: string): Promise<string> {
-    if (parameterName) {
-        console.log(parameterName);
-    }
-    return 'https://resmed-sandbox-dht.oktapreview.com/oauth2/aus16vrwhxyYOo1ko0h8';
-    // let result;
-    // try {
-    //     console.log(`start: getParameter(${parameterName})`);
-    //     result = await parameterStore
-    //         .getParameter({
-    //             Name: parameterName,
-    //         })
-    //         .promise();
-    //     console.log(`end: getParameter(${parameterName})`);
-    // } catch (err) {
-    //     console.error('ParameterStore error:', err);
-    //     throw err;
+    // if (parameterName) {
+    //     console.log(parameterName);
     // }
-    // const parameterValue = result.Parameter!.Value!;
+    // return 'https://resmed-sandbox-dht.oktapreview.com/oauth2/aus16vrwhxyYOo1ko0h8';
+    let result;
+    try {
+        console.log(`start: getParameter(${parameterName})`);
+        result = await parameterStore
+            .getParameter({
+                Name: parameterName,
+            })
+            .promise();
+        console.log(`end: getParameter(${parameterName})`);
+    } catch (err) {
+        console.error('ParameterStore error:', err);
+        throw err;
+    }
+    const parameterValue = result.Parameter!.Value!;
 
-    // return parameterValue;
+    return parameterValue;
 }
